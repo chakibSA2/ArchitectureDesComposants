4b8d792570dc018767e2f51590397e92960e5665
chakibSA2 <chakibsa2@gmail.com>
2025-02-07 19:40:08 +0100

    :white_check_mark: add srpingBoot  backend project

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,12 @@
-package com.example.projectbackend;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectBackendApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectBackendApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
-
 }
